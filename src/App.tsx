3d3a948caa4a2461c3b6c97a64aeae58abe4fdea
gautham <gaut1993@gmail.com>
2021-09-28 21:22:21 +0200

    add more functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 import './App.css';
 import {NotesComponent} from "./components/NotesComponent";
 
-
-//let noteList: Note[] = [];
-
-
 function App() {
-
     return (
         <NotesComponent/>
     );
