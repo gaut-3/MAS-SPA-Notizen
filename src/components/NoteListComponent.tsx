ef8f5d9f5268d39e7a7babcf9f18541e7d7a98c0
Gautham Arumugam <gaut1993@gmail.com>
2021-11-04 23:00:18 +0100

    sorting

--- src/components/NoteListComponent.tsx
+++ src/components/NoteListComponent.tsx
@@ -1,9 +1,10 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {Fragment, useState} from "react";
 import {Note} from "../models/Note";
-import {Button, Grid, Link, Typography} from "@mui/material";
+import {Grid, Link, Typography} from "@mui/material";
 import {NoteComponent} from "./NoteComponent";
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import {ArrowDownward} from "@mui/icons-material";
+import {NoteSortColumn} from "../models/NoteSortColumn";
+import {NoteSortOrder} from "../models/NoteSortOrder";
+import {NoteOrder} from "../models/NoteOrder";
 
 interface Props {
     noteList: Note[];
@@ -13,7 +14,10 @@ interface Props {
     deleteNote: (noteId: number) => void
     completeNote: (noteId: number, isComplete: boolean) => void
     changeNoteName: (noteId: number, noteName: string) => void
-    setSortType: (sortType: string) => void
+    setSortColumn: (sortColumn: keyof Note) => void
+    setSortType: (sortOrder: string) => void
+    setSortOrder: (noteOrder: NoteOrder) => void
+    sortOrder: NoteOrder
 }
 
 
@@ -25,37 +29,74 @@ export const NoteListComponent = ({
                                       completeNote,
                                       setNotePriority,
                                       changeNoteName,
-                                      setSortType
+                                      setSortColumn,
+                                      setSortType,
+                                      sortOrder,
+                                      setSortOrder
                                   }: Props) => {
 
     const [sortPriority, setSortPriority] = useState("")
     const [sortName, setSortName] = useState("")
 
-    const handleSortClick = (sortCloumn: string, event: React.MouseEvent) => {
 
+    const handleSortClick = (column: NoteSortColumn, event: React.MouseEvent) => {
+        const icon = switchSort(sortOrder.icon);
+        let noteSortOrder = {
+            sortColumn: column,
+            sortOrder: NoteSortOrder.ASC,
+            icon: icon,
+        }
+        setSortOrder(noteSortOrder)
+        //resetSorting();
+       // setSortColumn(NoteSortColumn.NAME);
+        //changeSortOrder(NoteSortColumn.NAME, NoteSortOrder.ASC);
+        //setSortName(switchSort(sortName));
     }
 
+    const resetSorting = () => {
+        setSortPriority("");
+        setSortName("");
+    }
 
-    const switchSort = (sortIcon: string): string =>  {
-        if(sortIcon === "sort-icon-up") {
+    const switchSort = (sortIcon: string): string => {
+        if (sortIcon === "sort-icon-up") {
             return "sort-icon-down";
-        } else if(sortIcon ===  "sort-icon-down") {
+        } else if (sortIcon === "sort-icon-down") {
             return "sort-icon-up";
         } else {
             return "sort-icon-up";
         }
     }
 
+    const header = () => {
+       /* if (noteList.length > 0) {
+            return (
+                <Fragment>
+                    <Grid item style={{textAlign: "right"}} xs={4}>
+                        <Link onClick={e => handleSortClick(NoteSortColumn.PRIORITY, e)} underline="none"><Typography
+                            className={"sort-icon " + sortPriority}>Wichtigkeit</Typography></Link>
+                    </Grid>
+                    <Grid style={{textAlign: "left"}} item xs={8}>
+                        <Link onClick={e =>  handleSortClick(NoteSortColumn.PRIORITY, e)} underline="none"><Typography
+                            className={"sort-icon " + (sortOrder.sortColumn === NoteSortColumn.PRIORITY ? sortOrder.icon : "")}>Aufgabe</Typography></Link>
+                    </Grid>
+                </Fragment>)
+        }*/
+    }
+
     return (
         <Grid container spacing={2} rowSpacing={2} alignItems="center" direction="row" justifyContent="center">
-
-            <Grid item style={{textAlign: "right"}} xs={4}>
-                <Link onClick={e => handleSortClick("priority", e)} underline="none"><Typography className={"sort-icon " + sortPriority}>Wichtigkeit</Typography></Link>
-            </Grid>
-            <Grid style={{textAlign: "left"}} item xs={8}>
-                <Link onClick={e => handleSortClick("priority", e)} underline="none"><Typography className={"sort-icon " + sortName}>Aufgabe</Typography></Link>
-            </Grid>
-
+            {noteList.length > 0 &&
+            <Fragment>
+                <Grid item style={{textAlign: "right"}} xs={4}>
+                    <Link onClick={e => handleSortClick(NoteSortColumn.PRIORITY, e)} underline="none"><Typography
+                        className={"sort-icon " + (sortOrder.sortColumn === NoteSortColumn.PRIORITY ? sortOrder.icon : "")}>Wichtigkeit</Typography></Link>
+                </Grid>
+                <Grid style={{textAlign: "left"}} item xs={8}>
+                    <Link onClick={e => handleSortClick(NoteSortColumn.NAME, e)} underline="none"><Typography
+                        className={"sort-icon " + (sortOrder.sortColumn === NoteSortColumn.NAME ? sortOrder.icon : "")}>Aufgabe</Typography></Link>
+                </Grid>
+            </Fragment>}
             {noteList.filter(note => {
                     if (searchParam !== "") {
                         return note.name.toLowerCase().includes(searchParam.toLowerCase())
